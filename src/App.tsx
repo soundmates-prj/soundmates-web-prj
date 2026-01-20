66c06dbc36ad40f8229ecbee036c444855265fc1
TranHoangNam177 <tranhoangnam1707@gmail.com>
2026-01-20 14:33:08 +0700

    Resolve config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home/Home';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
