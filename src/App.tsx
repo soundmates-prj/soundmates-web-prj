93299f302715769cd654a7d010d1d760c9c7ba1a
DucPPhan <144339332+DucPPhan@users.noreply.github.com>
2026-01-20 14:32:48 +0700

    fix code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import Login from './pages/Auth/Login';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   )
