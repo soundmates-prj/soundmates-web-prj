e92fb15eda36ec0aea04747ee08be9bca36867c5
Trần Hoàng Nam <Tranhoangnam1707@gmail.com>
2026-02-09 12:22:31 +0700

    Fix UI Register page

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -9,9 +9,9 @@ const api = axios.create({
 
 api.interceptors.request.use(
   function (config) {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
     }
     return config;
   },
